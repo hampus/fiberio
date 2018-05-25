258561e384f0ce5b8f5b2950cae9c5f4faa7a24d
Hampus Wessman <hampus.wessman@gmail.com>
2018-05-25 20:35:58 +0200

    Add generated Doxygen documentation

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['io_5ferror',['io_error',['../classfiberio_1_1io__error.html',1,'fiberio::io_error'],['../classfiberio_1_1io__error.html#aa42972b5f78c883e873118536e3e0f5a',1,'fiberio::io_error::io_error()']]]
+  ['io_5ferror',['io_error',['../classfiberio_1_1io__error.html',1,'fiberio::io_error'],['../classfiberio_1_1io__error.html#aa42972b5f78c883e873118536e3e0f5a',1,'fiberio::io_error::io_error()']]],
+  ['is_5fopen',['is_open',['../classfiberio_1_1socket.html#a4448735cbcea844127703c19d67cd7ca',1,'fiberio::socket']]]
 ];
