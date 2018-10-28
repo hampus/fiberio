9c927f43a00527910754ceaafa394e95c1c081ad
Hampus Wessman <hampus.wessman@gmail.com>
2018-10-28 14:49:27 +0100

    Regenerate Doxygen

--- docs/html/search/functions_2.js
+++ docs/html/search/functions_2.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['close',['close',['../classfiberio_1_1detail_1_1socket__device.html#a12e21e41f72e85a92336a89a9c943004',1,'fiberio::detail::socket_device::close()'],['../classfiberio_1_1server__socket.html#adea9be2287382df832381efd4ee7e720',1,'fiberio::server_socket::close()'],['../classfiberio_1_1socket.html#a0bfc24418859bd2a1845ea6b5733a568',1,'fiberio::socket::close()']]],
-  ['connect',['connect',['../classfiberio_1_1socket.html#aca5aa4d6b61abd6806d8a84fce3e11c5',1,'fiberio::socket']]],
-  ['connect_5fstream',['connect_stream',['../namespacefiberio.html#af4018074c8be6628853563b331c25d72',1,'fiberio']]]
+  ['connect',['connect',['../classfiberio_1_1socket.html#aca5aa4d6b61abd6806d8a84fce3e11c5',1,'fiberio::socket']]]
 ];
